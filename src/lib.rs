492322f7a73fb052b9893ffa10784130295aba78
bad_indentation <ulrichjohnsen314@gmail.com>
2026-06-05 18:27:14 -0400

    test: add failing test for wordlist loading.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
-pub mod rumble {
+use std::collections::HashSet;
 
+/// Load words from the given filepath
+pub fn load_words(path: &str) -> HashSet<String> {
+   return HashSet::new(); 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_word_loading() {
+        let mut expected: HashSet<String> = HashSet::new();
+        expected.extend(["word1", "word2", "anotherword"].map(String::from));
+        assert_eq!(load_words("public/test_list.txt"), expected);
+    }
 }
