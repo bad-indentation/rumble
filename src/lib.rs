a74ddcb597f6a67903234b25e3b7535123761c73
bad_indentation <ulrichjohnsen314@gmail.com>
2026-06-05 19:10:23 -0400

    create word list reader

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, fs};
 
-/// Load words from the given filepath
-pub fn load_words(path: &str) -> HashSet<String> {
-   return HashSet::new(); 
+/// Loads words from the given filepath
+/// Returns Err variant if file cannot be read
+pub fn load_words(path: &str) -> Result<HashSet<String>, std::io::Error> {
+    let mut words = HashSet::new();
+    let lines = fs::read_to_string(path)?;
+    
+    for word in lines.split_whitespace() {
+        words.insert(String::from(word).to_lowercase());
+    }
+
+    return Ok(words); 
 }
 
 #[cfg(test)]
@@ -13,6 +21,14 @@ mod tests {
     fn test_word_loading() {
         let mut expected: HashSet<String> = HashSet::new();
         expected.extend(["word1", "word2", "anotherword"].map(String::from));
-        assert_eq!(load_words("public/test_list.txt"), expected);
+        assert_eq!(load_words("public/test_list.txt").unwrap(), expected);
+    }
+
+    #[test]
+    fn test_failed_word_loading() {
+        assert!(match load_words("public/nonexistent.txt") {
+            Ok(_) => false,
+            Err(_) => true,
+        });
     }
 }
