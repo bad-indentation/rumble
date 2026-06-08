fdd30f32312cc818ab442979731081a3f2ae096e
bad_indentation <ulrichjohnsen314@gmail.com>
2026-06-07 22:48:34 -0400

    add pluralization to solution count

--- src/lib.rs
+++ src/lib.rs
@@ -69,71 +69,6 @@ fn load_words(
     Ok(words)
 }
 
-/// Returns the set of all unique prefixes within the set.
-fn get_prefixes(words: &HashSet<String>) -> HashSet<String> {
-    let mut prefixes = HashSet::new();
-
-    for word in words {
-        for end in 1..=word.len() {
-            prefixes.insert(String::from(&word[0..end]));
-        }
-    }
-
-    prefixes
-}
-
-/// Represents a potential word as it is being constructed via DFS
-struct PathBuilder {
-    word: String,
-    letters_available: Vec<char>,
-}
-
-impl PathBuilder {
-    /// Creates a new path from the given scrambled word
-    fn from(scrambled: &str) -> Self {
-        PathBuilder {
-            word: String::new(),
-            letters_available: scrambled.chars().collect(),
-        }
-    }
-
-    fn new(word: String, letters_available: Vec<char>) -> Self {
-        PathBuilder {
-            word,
-            letters_available,
-        }
-    }
-}
-
-/// Returns all valid prefixes that can be made using the scrambled letters in `word_path`
-/// TODO: get rid of all the clones
-fn get_words(word_path: PathBuilder, valid_prefixes: &HashSet<String>) -> HashSet<String> {
-    if word_path.letters_available.is_empty() {
-        return HashSet::from([word_path.word]);
-    }
-
-    let mut found: HashSet<String> = HashSet::new();
-    let mut new_path;
-
-    for (i, letter) in word_path.letters_available.iter().enumerate() {
-        let mut new_word = word_path.word.clone();
-        new_word.push(*letter);
-
-        if valid_prefixes.contains(&new_word) {
-            let mut new_letters = word_path.letters_available.clone();
-            new_letters.remove(i);
-
-            found.insert(new_word.clone());
-
-            new_path = PathBuilder::new(new_word.to_string(), new_letters);
-
-            found.extend(get_words(new_path, valid_prefixes).iter().cloned());
-        }
-    }
-
-    found
-}
-
 /// Configuration for command line args Parser
 /// Based on example in Clap documentation:
 /// https://docs.rs/clap/latest/clap/#example
@@ -196,7 +131,7 @@ pub fn run(config: Config) {
     if solutions == 0 {
         eprintln!("Sorry, couldn't find any solutions. :(");
     } else {
-        eprintln!("Found {solutions} solutions.");
+        eprintln!("Found {solutions} solution{}", "s".repeat(usize::min(1, solutions - 1)) + ".");
     } 
 }
 
@@ -270,33 +205,4 @@ mod tests {
         assert!(!is_anagram_of(candidate, &count, true));
     }
 
-    #[test]
-    fn test_get_prefixes() {
-        let mut words = HashSet::new();
-        words.extend(["apple", "banana"].map(String::from));
-
-        let mut expected_prefixes = HashSet::new();
-        expected_prefixes.extend(
-            [
-                "a", "ap", "app", "appl", "apple", "b", "ba", "ban", "bana", "banan", "banana",
-            ]
-            .map(String::from),
-        );
-
-        assert_eq!(get_prefixes(&words), expected_prefixes);
-    }
-
-    #[test]
-    fn test_get_words() {
-        let max_len = 100;
-        let letters = HashSet::from_iter("abcdefghijklmnopqrstuvwxyz12".chars());
-        let valid_prefixes =
-            get_prefixes(&load_words("public/test_list.txt", max_len, &letters).unwrap());
-        let results = get_words(PathBuilder::from("2owrd"), &valid_prefixes);
-
-        assert_eq!(
-            results,
-            HashSet::from(["w", "wo", "wor", "word", "word2"].map(String::from))
-        );
-    }
 }
